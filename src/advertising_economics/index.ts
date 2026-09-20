65b0388e8282c4ee006653af9e6526480f09d734
Hawxley <hawxley@gmail.com>
2026-09-20 15:23:14 -0400

    Step 5: expose evaluator oracle and adversarial fixtures

--- src/advertising_economics/index.ts
+++ src/advertising_economics/index.ts
@@ -3,3 +3,5 @@ export * from "./response.js";
 export * from "./delivery.js";
 export * from "./platform.js";
 export * from "./evaluator.js";
+export * from "./oracle.js";
+export * from "./adversarial.js";
