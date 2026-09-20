016a0f3c8fccdc5197f94483e2bdf74f78fa55e9
Hawxley <hawxley@gmail.com>
2026-09-20 17:04:35 -0400

    Step 7: expose God-mode ecommerce economics API

---
+++ src/ecommerce_economics/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./products.js";
+export * from "./waterfall.js";
+export * from "./returns.js";
+export * from "./evaluator.js";
+export * from "./counterfactual.js";
