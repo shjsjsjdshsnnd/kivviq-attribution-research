a609f862e6cdefcfdfa60af92dd6aa32667f8934
Hawxley <hawxley@gmail.com>
2026-09-20 17:14:25 -0400

    Step 7: expose God-mode opportunity and adversarial APIs

--- src/ecommerce_economics/index.ts
+++ src/ecommerce_economics/index.ts
@@ -4,3 +4,5 @@ export * from "./waterfall.js";
 export * from "./returns.js";
 export * from "./evaluator.js";
 export * from "./counterfactual.js";
+export * from "./opportunity.js";
+export * from "./adversarial.js";
